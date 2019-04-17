79bf74646dd41857f5d481096e98ae1fd745e9cd
pokeflutist78770 <a.aguayo30@gmail.com>
2019-04-17 14:49:11 -0700

    Initial item commit

---
+++ src/Items/Item.java
@@ -0,0 +1,7 @@
+package Items;
+
+
+public abstract class Item {
+	private static int COST;
+	private static int WEIGHT;
+}
---
+++ src/Items/Weapon.java
@@ -0,0 +1,18 @@
+package Items;
+
+public abstract class Weapon extends Item {
+	private static int DAMAGE;
+	private static int RANGE;
+	
+	/**
+	 * Performs an attack with this weapon. This method takes a current position
+	 * that is where the attack will be performed from, as well as a direction for the
+	 * attack. It then calculates is anything is located within the range of the weapon,
+	 * and deals the attack onto the items.
+	 * @return boolean determining if the attack was successful.
+	 */
+	public boolean attactk() {
+		
+	}
+	
+}
