403c2480eb7e1b3a07ec154593d759861a0e573d
Tito Vasquez <gjvasquez@email.arizona.edu>
2019-04-17 15:10:02 -0700

    Added controller

---
+++ src/controller/GameController.java
@@ -0,0 +1,23 @@
+package controller;
+
+/**
+ * Controller for the GameBoard including interactions with the player and enemies, etc.
+ * @author Tito Vasquez
+ *
+ */
+
+public class GameController {
+	
+	//collisions
+	
+	//movement
+	
+	//
+
+	//drop chances
+	
+	//attack
+	
+	//speak
+	
+}
