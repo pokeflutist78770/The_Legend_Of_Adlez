62c88c33be743c41710974fb2c41066a5017aca8
Tito Vasquez <gjvasquez@email.arizona.edu>
2019-04-17 14:54:02 -0700

    Most of the character classes. Still need some work with data.

---
+++ src/NPC.java
@@ -0,0 +1,30 @@
+/**
+ * Class for the OverWorld object type "NPC".
+ * @author Tito Vasquez
+ *
+ */
+public class NPC extends OverWorldClass {
+	String message;
+
+	/**
+	 * Constructs a NPC object and sets the image of it on the overworld map.
+	 * @param position int[] The position of the NPC object.
+	 */
+	public NPC(int[] position) {
+		super(position);
+		this.setImage(null);
+	}
+
+	@Override
+	public String getText() {
+		return message;
+	}
+	
+	/**
+	 * Setter for the message for a particular NPC.
+	 * @param message String The message that the NPC will say when they are spoken to.
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
