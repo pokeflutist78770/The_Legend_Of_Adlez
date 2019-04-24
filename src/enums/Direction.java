d78baf9dda7d6a2fa1bfe2ceb2095530b6edb1f8
Timothy Lyons <tdlyons@email.arizona.edu>
2019-04-23 17:10:31 -0700

    Fixed enum package name

--- src/enums/Direction.java
+++ src/enums/Direction.java
@@ -1,5 +1,12 @@
 package enums;
 
-public enum Direction{
+
+/**
+ * Direction enum. This allows each CharacterClass to store what direction they
+ * are currently facing, and is used for various actions with that CharacterClass
+ * @author ANgel Aguayo
+ *
+ */
+public enum Direction {
 	NORTH, SOUTH, EAST, WEST;
 }
