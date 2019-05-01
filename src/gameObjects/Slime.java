699815c732d303b2d5def1b8589d2b518e6e1e94
Tito Vasquez <39856253+gjvasquez@users.noreply.github.com>
2019-04-30 18:57:39 -0700

    merge

--- src/gameObjects/Slime.java
+++ src/gameObjects/Slime.java
@@ -14,7 +14,7 @@ public class Slime extends Enemy {
 	 * @param currentHP int The currentHP of the Slime.
 	 * @param totalHP int The totalHP of the Slime.
 	 * @param attack int The attack power of the Slime.
-	 * @param position int[] The current position of the Slime.
+	 * @param position Point2D The current position of the Slime.
 	 */
 	public Slime(int currentHP, int totalHP, int attack, Point2D position) {
 		super(currentHP, totalHP, attack, position);
@@ -22,12 +22,13 @@ public class Slime extends Enemy {
 
 	/**
 	 * Separate constructor that hard codes in the stats of the Slime for consistency.
-	 * @param position int[] The current position of the Slime on the game map.
+	 * @param position Point2D The current position of the Slime on the game map.
+	 * @param n int The type of movement pattern that the Slime will have.
 	 */
-	public Slime(Point2D position) {
-		super(position);
-		this.setCurrentHP(10);
+	public Slime(Point2D position, int n) {
+		super(position, n);
+		this.setHP(10);
 		this.setAttack(1);
-		this.setImage(null);		
+		this.setImage("assets/slime.png");		
 	}
 }
